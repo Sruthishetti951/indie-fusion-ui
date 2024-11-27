aa11b15de245ec50d5b1a9b2699ab7a1a9448aa6
sruthi kami <sruthi@gmail.com>
2024-11-26 22:36:50 -0500

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as servericeWorker from './service-worker';
+// import * as servericeWorker from './service-worker1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,4 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-servericeWorker.register();
+// servericeWorker.register();
